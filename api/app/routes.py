4dc433649109add48e3156d8e40048219f34a512
namedmichi <j.technology@web.de>
2024-01-13 22:48:18 +0100

    commit

---
+++ api/app/routes.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, request, make_response
+from app import app
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI()
+
+# Endpoint for gpt chat
+@app.route("/api/chat", methods=["POST"])
+def chat():
+    data = request.get_json()
+
+
+
+    messages = data["messages"]
+    model = data["model"]
+    maxTokens = int(data["maxTokens"])
+    temperatur = float(data["temperature"])
+    stop = data["stop"] if data["stop"] else None
+    top_p = data["top_p"] if data["top_p"] else 1
+    frequency_penalty = data["frequency_penalty"] if data["frequency_penalty"] else 0
+    seed = data["seed"] if data["seed"] else None
+    
+
+    if(seed == None):
+        response = client.chat.completions.create(
+
+            messages=messages,
+            model=model,
+            max_tokens=maxTokens,
+            temperature=temperatur,
+            top_p=top_p,
+            frequency_penalty=frequency_penalty,
+            stop=stop
+        )
+    else:
+        response = client.chat.completions.create(
+
+            messages=messages,
+            model=model,
+            max_tokens=maxTokens,
+            temperature=temperatur,
+            top_p=top_p,
+            frequency_penalty=frequency_penalty,
+            stop=stop,
+            seed=seed
+        )
+
+    text = response.choices[0].message.content
+
+    response = make_response(jsonify(text), 200)
+   
+    return response
